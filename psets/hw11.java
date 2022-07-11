b11f6f190b0e56bdddc88c4b3ed864aee6ea3c9f
Brandon Tran <brandontran@Brandons-MacBook-Air.local>
2022-07-10 19:56:14 -0700

    fixed formatting

--- psets/hw11.java
+++ psets/hw11.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class hw11 {
     
-
     //Write the method areaOfPolygon that takes an array of (x,y) points (each coordinate pair is also an array) that are guaranteed to be in either clockwise or counter-clockwise order around a polygon, and returns the area of that polygon, as described here. For example (taken from that text), areaOfPolygon({{4,10}, {9,7}, {11,2}, {2,2}}) returns 45.5 (at least the result is almostEqual to 45.5).
     public static void areaOfPolygon(int[][] arr){
 
@@ -15,7 +14,6 @@ public class hw11 {
 
     }
 
-
     /*A polynomial can be represented as an array of its coefficients. For example, {2, 3, 0, 4} could represent the polynomial 2x^3 + 3x^2 + 4. With this in mind, write the method multiplyPolynomials which takes two integer arrays representing polynomials as just described, and returns an integer ArrayList  representing the polynomial which is the product of the two. For example, multiplyPolynomials({2,0,3}, {4,5}) represents the problem (2x^2 + 3)(4x + 5), and:
     (2x^2 + 3)(4x + 5) = 8x^3 + 10x^2 + 12x + 15
     And so this returns the ArrayList {8, 10, 12, 15}.*/
@@ -24,18 +22,17 @@ public class hw11 {
 
     }
     
-
     //Write the method alternatingSum that takes an array of numbers and returns the alternating sum (where the sign alternates from positive to negative or vice versa). For example, alternatingSum({5,3,8,4}) returns 6 (that is, 5-3+8-4). If the list is empty, return 0.
     public static void alternatingSum(int[] arr){
 
     }
 
     /*Write the function smallestDifference that takes an array of integers and returns the smallest absolute difference between any two integers in the list. If the list is empty, return -1. For example:
-    smallestDifference({19,2,83,6,27{}) should return 4 since the two closest numbers in that list are 2 and 6, and their difference is 4.
-    */
+    smallestDifference({19,2,83,6,27{}) should return 4 since the two closest numbers in that list are 2 and 6, and their difference is 4.*/
     public static void smallestDifference(int[] arr){
 
     }
+    
     //Helper function; ignore this
     public static boolean almostEqual(double d1, double d2){
 
@@ -135,6 +132,7 @@ public class hw11 {
         assert smallestDifference(arr3) == 1;
         System.out.print("Passed!");
     }
+
     public static void main(String[] args){
 
     }
